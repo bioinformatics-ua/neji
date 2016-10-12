95316a44ba3b9d42c5f2c5eff1dfb88fcaf7e38d
David Campos <david.campos@bmd-software.com>
2016-10-12 16:30:07 +0100

    Neji is now available at https://github.com/BMDSoftware/neji.

--- neji.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cp=target/neji-1.0-SNAPSHOT-jar-with-dependencies.jar:$CLASSPATH
-MEMORY=4G
-JAVA_COMMAND="java -Xmx$MEMORY -Dfile.encoding=UTF-8 -classpath $cp"
-CLASS=pt.ua.tm.neji.cli.Main
-
-$JAVA_COMMAND $CLASS $*
